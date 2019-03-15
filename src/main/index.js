fae29b1baa9afa62e8fd3d74766b81749ef45e2e
Reni Dantas <renidanta0s@gmail.com>
2019-03-15 15:20:12 -0300

    Menubar only with alt key

--- src/main/index.js
+++ src/main/index.js
@@ -4,7 +4,8 @@ const path = require('path');
 app.on('ready', () => {
     win = new BrowserWindow({width: 800, height: 600, 
                             show: false, icon: path.join(__dirname, '../../resources/icon.png'),
-                            resizable: false});
+                            resizable: false,
+                            autoHideMenuBar: true});
     // win.setMenu(null);
     win.loadURL('file:///' + path.resolve(__dirname, '../renderer/index.html'));
     win.on('ready-to-show', () => {
